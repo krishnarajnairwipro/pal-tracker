30e58f7e5cdba766526b879fa59cc58bd88e933d
unknown <IMGADMIN@D-113109649.wipro.com>
2019-04-08 13:58:30 +0530

    Simple .NET Core Web API app 3

--- src/PalTracker/WelcomeController.cs
+++ src/PalTracker/WelcomeController.cs
@@ -6,6 +6,6 @@ namespace PalTracker
     public class WelcomeController : ControllerBase
     {
         [HttpGet]
-        public string SayHello() => "hello madhu";
+        public string SayHello() => "hello";
     }
 }
